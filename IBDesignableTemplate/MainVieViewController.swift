6ee1db806b703ba584e705754f325f45c64879e6
Giovanne Bressam <giovannebressam@gmail.com>
2021-12-19 22:21:57 -0300

    Initial commit

---
+++ IBDesignableTemplate/MainVieViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainVieViewController.swift
+//  IBDesignableTemplate
+//
+//  Created by Giovanne Bressam on 19/12/21.
+//
+
+import UIKit
+
+class MainVieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
